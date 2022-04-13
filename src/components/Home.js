6e90946606fc4bb1a79bba91194ac69769080e4d
Kevin <kevin.horng422@gmail.com>
2022-04-13 16:35:57 -0400

    Home page finished

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,13 +8,19 @@ function Home() {
       <div className='home__container'>
         <img className='home__image' src='https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg' alt=''/>
         <div className='home__row'>
-          <Product />
+          <Product title='The Lean' price='29.99' rating='***'/>
+          <Product title='Caffeine' price='29.99' rating='**'/>
+          <Product title='Weights' price='200.10' rating='*'/>
         </div>
         <div className='home__row'>
-          {/* Products */}
+          <Product title='The Lean' price='29.99' rating='***'/>
+          <Product title='Caffeine' price='29.99' rating='**'/>
+          <Product title='Weights' price='200.10' rating='*'/>
         </div>
         <div className='home__row'>
-          {/* Products */}
+          <Product title='The Lean' price='29.99' rating='***'/>
+          <Product title='Caffeine' price='29.99' rating='**'/>
+          <Product title='Weights' price='200.10' rating='*'/>
         </div>
       </div>
     </div>
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,17 +1,18 @@
 import React from 'react'
+import './Product.css'
 
-function Product() {
+function Product({title, image, price, rating}) {
   return (
     <div className='product'>
         <div className='product__info'>
-            <p>This could be anything, this could also be pulled from a JSON data file</p>
+            <p>{title}</p>
             <p className='product__price'>
                 <small>$</small>
-                <strong>19.99</strong>
+                <strong>{price}</strong>
             </p>
         </div>
         <div className='product__rating'>
-            <p>*</p>
+            <p>{rating}</p>
         </div>
         <img className='product__image' src='' alt='' />
         <button>Add to basket</button>
