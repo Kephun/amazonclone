011e944fd09f5241924ab585c7b834c0bf91caa6
Kevin <kevin.horng422@gmail.com>
2022-04-18 16:34:36 -0400

    Started the authentication process through firebase

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { Link } from 'react-router-dom';
+import { db, auth } from './firebase'
 
 function Login() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = (e) => {
+    e.preventDefault();
+    //Firebase authentication
+  }
+
+  const register = (e) => {
+    e.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        console.log(auth)
+      })
+    //do some fancy firebase register
+  }
+
   return (
     <div className='login'>
       <Link to='/'>
@@ -12,13 +32,13 @@ function Login() {
         <h1>Sign-in</h1>
         <form>
           <h5>Email</h5>
-          <input type='text'/><br/>
+          <input type='text' value={email} onChange={e => setEmail(e.target.value)}/><br/>
           <h5>Password</h5>
-          <input type='password'/>
+          <input type='password' value={password} onChange={e => setPassword(e.target.value)}/>
         </form>
-        <button className='login__signInButton'>Sign in</button>
+        <button type='submit' className='login__signInButton' onClick={signIn}>Sign in</button>
         <p>By signing-in you agree to Amazon's Condition of Use and Sale</p>
-        <button className='login__registerButton'>Create Your Amazon Account</button>
+        <button className='login__registerButton' onClick={register}>Create Your Amazon Account</button>
       </div>
     </div>
   )
---
+++ src/pages/firebase.js
@@ -0,0 +1,19 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCvEEvDCjowyz75J1GCh82nLAcFZL9v9fE",
+  authDomain: "clone-57120.firebaseapp.com",
+  projectId: "clone-57120",
+  storageBucket: "clone-57120.appspot.com",
+  messagingSenderId: "1078364735275",
+  appId: "1:1078364735275:web:cbdcaae3ea3de90609276e",
+  measurementId: "G-5TD40JSS8W"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth }; 
