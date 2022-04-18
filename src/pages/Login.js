533171a117eb44c9424668304e3e4ead51e2603b
Kevin <kevin.horng422@gmail.com>
2022-04-18 15:33:06 -0400

    Finished the UI for the login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,25 @@
-import React from 'react'
-import './Login.css'
+import React from 'react';
+import './Login.css';
+import { Link } from 'react-router-dom';
 
 function Login() {
   return (
     <div className='login'>
-      <h2>Sign in</h2>
-      <form className='login__form'>
-        <input type='text'/><br/>
-        <input type='text'/>
-      </form>
-      <button>Sign in</button>
-      <small><p>By signing-in you agree to Amazon's Condition of Use and Save</p></small>
-      <button>Create Your Amazon Account</button>
+      <Link to='/'>
+        <img className='login__logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' alt=''/>
+      </Link>
+      <div className='login__container'>
+        <h1>Sign-in</h1>
+        <form>
+          <h5>Email</h5>
+          <input type='text'/><br/>
+          <h5>Password</h5>
+          <input type='password'/>
+        </form>
+        <button className='login__signInButton'>Sign in</button>
+        <p>By signing-in you agree to Amazon's Condition of Use and Sale</p>
+        <button className='login__registerButton'>Create Your Amazon Account</button>
+      </div>
     </div>
   )
 }
